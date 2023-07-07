d7c8df69cacf0dc578ef39a9a5ec72f7d5b48614
Woronetski_AO <woronetskianton@gmail.com>
2023-07-07 16:34:52 +0300

    Init

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+declare(strict_types=1);
+
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+require_once "./src/AbstractFabric/index.php";
+echo "</br>";
+require_once "./src/FabricMethod/index.php";
